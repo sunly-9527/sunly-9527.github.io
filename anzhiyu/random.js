939c6aea4f4622535f9f9427c283fd585802d418
sunly-9527 <13767628954@163.com>
2025-03-30 22:28:16 +0800

    Site updated: 2025-03-30 22:28:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/22/这是一篇新的博文/"];function toRandomPost(){
+var posts=["2025/03/30/告别复制粘贴！Cursor编辑器：让你和代码“聊”起来，开发效率原地起飞！/","2025/02/22/深入解析Vue响应式系统的设计与实现/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
